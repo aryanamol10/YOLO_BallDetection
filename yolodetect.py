cffbbcf0465eed2c55261e4130eb2418385f1926
ashgreninja10 <Aryan.Kulkarni29@bcp.org>
2026-02-10 15:11:08 -0800

    Fixed up distance logic to be able to calculate speed through x and y coordinates

--- yolodetect.py
+++ yolodetect.py
@@ -4,45 +4,44 @@ import math
 
 cap = cv2.VideoCapture(0)
 
+FPS = 120
+cap.set(cv2.CAP_PROP_FPS, FPS)
+
 ret, frame = cap.read()
 
 model = YOLO("PyTorch_model.pt")
 
-ball_num = 0
 #calibrated value
 focal_length = 1755
-ball_w = 0.4925
+ball_w = 0.04
 IMG_WIDTH = 1280 
 CX = IMG_WIDTH / 2
 
-conversion_factor = 0.3048
-detected_objects = []
 
-def rundistcheck(x1, x2):
-
-    #First, we need to find the focal length
-    w_pixels = x2 - x1
-
-    
-    centerx = (x1+x2)/2
-    distance_feet = 2
-
-    dz = (ball_w*focal_length)/w_pixels
-
-    dx = ((centerx - CX)*dz)/focal_length
-
-    detected_objects.append((dz, dx))
+detected_objects = [(0,0)]
 
 
 
-def checkdetect(ball_num, x1, x2):
-    ball_num+=1
-    if (ball_num-1)>1:
-        print("multiple balls")
-    elif (ball_num-1)==1:
-        print("Only one ball")
-    elif (ball_num)-1==0:
-        print("zero balls")
+def runspeedcheck(x1, y1):
+
+    detected_objects.insert(0, (x1, y1))
+
+        #easy to check through
+    b1 = detected_objects[0]
+    b2 = detected_objects[1]
+
+        #distance formula
+    distance = math.sqrt((b2[0] - b1[0])**2 + (b2[1] - b1[1])**2)
+    time = 1000/FPS
+
+    detected_objects.pop(-1)
+
+    speed = distance/time
+
+    if(speed <= 0.2):
+        return 0
+    else:
+        return speed
     
 
 def feet2m(feet):
@@ -79,13 +78,10 @@ while True:
             # Extract coordinates in xyxy format
             x1, y1, x2, y2 = map(int, box.xyxy[0])
 
-            rundistcheck(x1, x2)
-
             # Extract confidence and class ID
             confidence = box.conf[0]
             cls_id = int(box.cls[0])
             class_name = names[cls_id]
-            pixels = (x2 - x1)+(y2-y1)
             # Create the label text
             #label = f"Conf: {confidence:.2f} | {feet2m(380/pixels):.2f} | {pixels:.2f}m"
 
@@ -97,30 +93,11 @@ while True:
 
             # Put the label text above the bounding box
             #cv2.putText(gray_3_channel, (x1, max(y1 - 10, 20)), cv2.FONT_HERSHEY_SIMPLEX, 0.6, color, 2)
-
-            ball_num+=1
-
-        if len(detected_objects)>=2:
-            #easy to check through
-            b1 = detected_objects[0]
-            b2 = detected_objects[1]
-
-            #distance formula
-            gap_ft = math.sqrt((b2[0] - b1[0])**2 + (b2[1] - b1[1])**2)
-
-            gap_ft_zero = gap_ft<=0.5
-            if(gap_ft_zero):
-                gap_ft = 0
-                
-            gap_m = gap_ft * conversion_factor
-
         
             #print(f"Gap: {gap_ft:.2f} ft | {gap_m:.2f} m")
-            cv2.putText(frame, f"Dist: {gap_ft:.2f}ft | Dist: {gap_m:.2f}m", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
+            cv2.putText(frame, f"Dist: {runspeedcheck(x1, y1):.2f}m/s ", (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 3)
 
         #checkdetect(ball_num, current_x1, current_x2)
-        detected_objects.clear()
-        ball_num = 0
     cv2.imshow("detections", gray_3_channel)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
